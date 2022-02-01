8cef99e965ee5ce30e7c1ec592e248fd5d49d27f
abhaynit <87191075+abhaynit@users.noreply.github.com>
2022-02-01 18:23:59 +0530

    h

--- WEB SCRAPPING/covid/flipkart/asd.py
+++
@@ -1,7 +0,0 @@
-import os
-path_to_data = "C:/Users/abhay/Pictures/desired/shoe/"
-result = []
-for entry in  os.scandir(path_to_data):
-    print(entry.path)
-    ab = entry.path
-    print('media/'+ ab.split('/')[-1])
